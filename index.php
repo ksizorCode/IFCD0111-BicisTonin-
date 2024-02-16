4486aec523710c6b3e75fc441105c5faa2cdb5bb
Miguel Esteban <miguelestebn@gmail.com>
2024-02-16 09:01:21 +0100

    aaa

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bicicletas Toñin</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header>
+            <p>Bicicletas Toñin</p>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="productos.php">Productos</a></li>
+                    <li><a href="cotacto.php">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <h1>Bienvenidos a Bicicletas Toñin</h1>
+            <p>En Bicicletas Toñin somos espexpertos en Bicicletas de Montaña. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, error porro? Nisi, dicta libero? Quibusdam, culpa placeat? Eligendi adipisci quae voluptatem expedita quibusdam ut earum. Architecto recusandae omnis voluptatem dolore!</p>
+            <img src="img/bicicleta.jpg" alt="Bicicletas Toñin" title="Bicicletas Toñin">
+        </main>
+
+        <footer>
+            <p>&copy; Copyright 2024 - Bicicletas Toñin</p>
+        </footer>
+
+    </body>
+</html>
